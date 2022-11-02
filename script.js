cf16701ec782e83f41fe477332dc24313a605588
RubsRafa <rubiarafaela2000@gmail.com>
2022-11-02 16:32:53 -0300

    com tela1, tela2 e inicio da tela3

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function criarQuizz () {
     const telaCriarQuizz = document.querySelector ('.criarQuizzConteudo');
     telaCriarQuizz.classList.remove ('esconder');
 }
+function abrirQuizz (item) {
+    console.log (item)
+    const esconderEntrada = document.querySelector ('.entrada');
+    esconderEntrada.classList.add ('esconder');
+    const abrirQuizz = document.querySelector ('.paginaQuizz');
+    abrirQuizz.classList.remove ('esconder');
+}
+
+//para cada um dos 4 quadrados do quizz, terá que ter uma função selecionarOpcao; 
+function selecionarOpcao(respostaSelecionada) {
+    console.log (respostaSelecionada);
+
+}
